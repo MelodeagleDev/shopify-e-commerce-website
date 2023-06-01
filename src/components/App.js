7b295bed8dc709e50d1d2921a3ea4bbba8dbb003
unknown <AokiNorio0825@gmail.com>
2023-06-01 13:14:09 +0900

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './header/Header';
+import NavBar from './header/NavBar';
+
+
+import '../styles/bootstrap.css';
+import '../styles/App.scss';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/NavBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <nav>
+            <div className="row">
+                <ul>
+                    <li><a href="#All"><ion-icon style={{
+                        fontSize: "30px",
+                        marginRight: "10px"
+                        }} name="menu-outline"></ion-icon> All Category <ion-icon name="chevron-down-outline" style={{marginLeft:"20px"}}></ion-icon></a></li>
+                    <li><a href="#home" class="nav-active scroll-easing">Home <ion-icon name="chevron-down-outline"></ion-icon></a></li>
+                    <li><a href="#Pages" class="scroll-easing">Pages <ion-icon name="chevron-down-outline"></ion-icon></a></li>
+                    <li><a href="#Products" class="scroll-easing">Products <ion-icon name="chevron-down-outline"></ion-icon></a></li>
+                    <li><a href="#Shop" class="scroll-easing">Shop <ion-icon name="chevron-down-outline"></ion-icon></a></li>
+                    <li><a href="#contact" class="scroll-easing">Contact Us </a></li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
